063b5a4a3b55cf893a983b314df6401cdc0ee413
escotoj <j.escoto14@gmail.com>
2023-04-27 09:50:19 -0700

    added lib folder and CLI file that includes the inquierer prompt with questions

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,30 @@
 DROP DATABASE IF EXISTS headcount_inv;
 CREATE DATABASE headcount_inv;
 
-USE headcount;
+USE headcount_inv;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    tile VARCHAR(30) NOT NULL,
+    salary DEC NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES department(id),
 );
